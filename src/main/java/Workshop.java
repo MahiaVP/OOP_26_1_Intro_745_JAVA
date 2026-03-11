b586b28eee42da271b43c1653d282cc061bf9a31
MahiaVP <sunnix035@gmail.com>
2026-03-11 13:36:53 -0500

    Rotar arreglo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -156,7 +156,7 @@ public class Workshop {
 
     // Método que elimina los duplicados de un arreglo
     public int[] eliminarDuplicados(int[] arreglo) {
-        //ni idea
+	
 	return new int[0];
     }
 
@@ -176,9 +176,16 @@ public class Workshop {
 
     // Método que rota un arreglo n posiciones
     public int[] rotarArreglo(int[] arreglo, int posiciones) {
-        // TODO: Implementar el método para rotar un arreglo n posiciones.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y posiciones = 2, el resultado debería ser [3, 4, 5, 1, 2].
-        return new int[0];
+        int j =0;
+	for(int i=0; i<posiciones;i++){
+		int t=arreglo[0];
+		while(j<arreglo.length){
+			arreglo[j]=arreglo[j+1];
+			j=j+1;
+		}
+		arreglo[arreglo.length-1]=t;
+	}
+        return arreglo;
     }
 
     // Método que cuenta los caracteres en una cadena
